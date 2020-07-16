b2a5454a26521099cfd5cdf2f17c19c77235effe
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-07-16 18:03:36 +0300

    extra files deleted

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '5f103d07a5bb9e03e0f861b6',
