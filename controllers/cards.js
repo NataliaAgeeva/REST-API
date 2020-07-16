85560d6dfd3f808dbbea0edc0a611a1244fe4721
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-07-16 20:38:02 +0300

    status of response for cards and users changed

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,20 +2,20 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((cards) => res.status(200).send({ data: cards }))
+    .catch(() => res.status(500).json({ message: 'Произошла ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => res.status(201).send({ data: card }))
+    .catch(() => res.status(500).json({ message: 'Произошла ошибка' }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => res.status(204).send({ data: card }))
+    .catch(() => res.status(404).json({ message: 'Произошла ошибка' }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -2,20 +2,20 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((users) => res.status(200).send({ data: users }))
+    .catch(() => res.status(500).json({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getOneUser = (req, res) => {
   User.findById(req.params.id)
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((users) => res.status(200).send({ data: users }))
+    .catch(() => res.status(404).json({ message: 'Произошла ошибка' }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((user) => res.status(201).send({ data: user }))
+    .catch(() => res.status(500).json({ message: 'Произошла ошибка' }));
 };
